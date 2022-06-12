d4131d85193e62b7d19887206cb75ed212ce2bfa
DraimDEV <danilaorlov4@gmail.com>
2022-06-13 00:35:33 +0300

    [1.0.5] HotbarEvent работает на нажатие по 9 слоту.

--- src/main/java/net/draimcido/draimrecipebook/Events/HotbarEvent.java
+++ src/main/java/net/draimcido/draimrecipebook/Events/HotbarEvent.java
@@ -1,32 +1,33 @@
 package net.draimcido.draimrecipebook.Events;
 
 import me.filoghost.chestcommands.api.ChestCommandsAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 
 public class HotbarEvent implements Listener {
 
     @EventHandler
-    public void InventoryClick (InventoryClickEvent event) {
-        if (event.getClickedInventory() == null) return;
-        if (event.getView().getBottomInventory().getType() == InventoryType.PLAYER) {
-            if (event.getSlot() == 8) {
-                    event.setCancelled(true);
-            }
+    public void HotbarEvent (PlayerItemHeldEvent event) {
+        Player p = (Player) event.getPlayer(); // Проверка на игрока
+        if (event.getNewSlot() == 8) { // Проверка слота
+            ChestCommandsAPI.openInternalMenu(p, "mir.yml"); // Открытие меню "mir.yml" через CC-API
+            event.isCancelled(); // Отмена последующих приминений(Чтобы меню открывалось онли на 9 слот хотбара)
         }
     }
 
 
+
     @EventHandler
-    public void InventoryOpenGUI (InventoryClickEvent event) {
-        if (event.getSlot() == 8) {
-            Player p = Bukkit.getPlayer("DanichMan");
-            ChestCommandsAPI.openInternalMenu(p, "mir.yml");
+    public void InvClick (InventoryClickEvent event) {
+        if (event.getClickedInventory() == null) return;
+        if (event.getView().getBottomInventory().getType() == InventoryType.PLAYER) {
+            if (event.getSlot() == 8) {
+                event.setCancelled(true);
+            }
         }
     }
 }
